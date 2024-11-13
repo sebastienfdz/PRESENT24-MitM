8666ed0ad602d7734a241baa79ce68b507fb1688
Sébastien F <sebastien.fernandez0@gmail.com>
2024-11-14 00:27:10 +0100

    bug-build: fixed the parser inputs (hexadecimal) and added a help message

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,34 @@ mod rust;
 use crate::rust::attack;
 
 
+/// Parse the input string to hexadecimal.
+fn hex_parser(s: &str) -> Result<u32, String> {
+    match u32::from_str_radix(s.trim_start_matches("0x"), 16) {
+        Ok(val) => Ok(val),
+        Err(e) => Err(format!("Failed to parse hexadecimal number '{}': {}", s, e)),
+    }
+}
+
+
 /// CLI Arguments for the Meet-in-the-Middle attack on PRESENT24
-#[derive(Parser)]
+#[derive(Parser, Debug)]
+/// Help message
+#[command(
+    name = "MitM Attack",
+    about = "PRESENT24 Meet-In-The-Middle attack",
+    override_usage = "cargo run [--release] [-- [--P <HEX>] [--cipher1 <HEX>] [--plain2 <HEX>] [--c <HEX>]]"
+)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short='P', long, value_parser=hex_parser, help="First plaintext (in hex format)")]
     plain1: Option<u32>,
 
-    #[arg(short, long)]
+    #[arg(short='C', long, value_parser=hex_parser, help="First cypher (in hex format)")]
     cipher1: Option<u32>,
 
-    #[arg(short, long)]
+    #[arg(short='p', long, value_parser=hex_parser, help="Second plaintext (in hex format)")]
     plain2: Option<u32>,
 
-    #[arg(short, long)]
+    #[arg(short='c', long, value_parser=hex_parser, help="Second cypher (in hex format)")]
     cipher2: Option<u32>,
 }
 
@@ -34,5 +49,5 @@ fn main() {
     let start = Instant::now();
     attack::mitm_attack_present24(plain1, cipher1, plain2, cipher2);
     let duration = start.elapsed();
-    println!("Durée totale de l'attaque : {:.1?}", duration);
+    println!("Total Meet-in-the-Middle attack time: {:.1?}", duration);
 }
